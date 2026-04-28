21fa802f571718c8c32c40a4143ff811c99b0707
kelvinb24 <Kelvinbediako7@gmail.com>
2026-04-28 12:19:23 -0400

    Add functions to create and destroy strings

--- string_object.c
+++ string_object.c
@@ -1,4 +1,19 @@
 #include "string_object.h"
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* createString(const char* src) {
+    if (src == NULL) return NULL;
+
+    char* str = malloc(strlen(src) + 1);
+    if (str == NULL) return NULL;
+
+    strcpy(str, src);
+    return str;
+}
+
+void destroyString(char* str) {
+    if (str != NULL) {
+        free(str);
+    }
+}
